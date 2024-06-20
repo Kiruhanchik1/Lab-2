40b0d5653c2dfd398b7dc8ad5e82b876899acf48
Kiruhanchik1 <cuzmitch.cyril@yandex.ru>
2024-06-20 18:37:15 +0300

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,22 @@
+echo off
+md Kirill
+cd Kuzmich
+md Kirill
+md Kuzmich
+pause
+cd Kirill
+echo > 15.txt
+cd..
+cd Kuzmich
+echo > 14022003.txt
+cd..
+cd..
+pause
+del Kuzmich /S /Q /F
+pause
+cd Kuzmich
+rd Kirill
+rd Kirill
+cd..
+rd Kirill
+pause
